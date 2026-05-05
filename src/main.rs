ea049803259004ff65810659b5300c147808c457
itsara <khajer@gmail.com>
2026-05-05 22:44:14 +0700

    add log lib

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use axum::{routing::get, Router};
 use dotenvy::dotenv;
+use log::{debug, info};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+
+    info!("Starting server...");
 
     let app = Router::new().route("/", get(hello_world));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Server running on http://localhost:3000");
+    info!("Server running on http://localhost:3000");
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn hello_world() -> &'static str {
+    debug!("hello_world handler called");
     "Hello, World!"
 }
